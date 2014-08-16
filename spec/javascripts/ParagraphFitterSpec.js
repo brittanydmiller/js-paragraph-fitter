97651cb1331bc75aea6e4f7ad7e98e4910f3fb91
Brittany Miller <brittanydmiller@gmail.com>
2014-08-16 11:58:45 -0700

    Adds jasmine tests that spy on function calls for paragraphFitter

--- spec/javascripts/ParagraphFitterSpec.js
+++ spec/javascripts/ParagraphFitterSpec.js
@@ -137,17 +137,25 @@ adventure.'[121]");
     spyOn(song, 'persistFavoriteStatus');
 
 
-    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
+
+  it("calls 'handleExceptions' from 'fitToWidth'", function() {
+    var paragraphText = "In August 2013, Gunn posted on his Facebook page that Tyler Bates would be composing the film's score. Gunn stated that Bates would write some of the score first so that he can film to the music, as opposed to scoring to the film.[129] In February 2014, Gunn revealed that the film would incorporate songs from the 1970s and 1980s, such as 'Hooked on a Feeling', on a mixtape in Quill's Walkman, which acts as a way for him to stay connected to the Earth, home and family he lost.[130] In May 2014, Gunn added that using the songs from the 70s and 80s were 'cultural reference points', saying, 'It’s striking the balance throughout the whole movie, through something that is very unique, but also something that is easily accessible to people at the same time. The music and the Earth stuff is one of those touchstones that we have to remind us that, yeah, [Quill] is a real person from planet Earth who’s just like you and me. Except that he’s in this big outer space adventure.'[121]"
+    spyOn(paragraphFitter, 'handleExceptions')
+    paragraphFitter.fitToWidth(widthInInches, paragraphText)
+    expect(paragraphFitter.handleExceptions).toHaveBeenCalled();
   });
 
-  //demonstrates use of expected exceptions
-  describe("#resume", function() {
-    it("should throw an exception if song is already playing", function() {
-      player.play(song);
+  it("calls 'insertBreaks' from 'fitToWidth'", function() {
+    var paragraphText = "In August 2013, Gunn posted on his Facebook page that Tyler Bates would be composing the film's score. Gunn stated that Bates would write some of the score first so that he can film to the music, as opposed to scoring to the film.[129] In February 2014, Gunn revealed that the film would incorporate songs from the 1970s and 1980s, such as 'Hooked on a Feeling', on a mixtape in Quill's Walkman, which acts as a way for him to stay connected to the Earth, home and family he lost.[130] In May 2014, Gunn added that using the songs from the 70s and 80s were 'cultural reference points', saying, 'It’s striking the balance throughout the whole movie, through something that is very unique, but also something that is easily accessible to people at the same time. The music and the Earth stuff is one of those touchstones that we have to remind us that, yeah, [Quill] is a real person from planet Earth who’s just like you and me. Except that he’s in this big outer space adventure.'[121]"
+    spyOn(paragraphFitter, 'insertBreaks')
+    paragraphFitter.fitToWidth(widthInInches, paragraphText)
+    expect(paragraphFitter.insertBreaks).toHaveBeenCalled();
+  });
 
-      expect(function() {
-        player.resume();
-      }).toThrowError("song is already playing");
-    });
+  it("calls 'findNextLineEnd' when 'insertBreaks' is called", function() {
+    var paragraphText = "In August 2013, Gunn posted on his Facebook page that Tyler Bates would be composing the film's score. Gunn stated that Bates would write some of the score first so that he can film to the music, as opposed to scoring to the film.[129] In February 2014, Gunn revealed that the film would incorporate songs from the 1970s and 1980s, such as 'Hooked on a Feeling', on a mixtape in Quill's Walkman, which acts as a way for him to stay connected to the Earth, home and family he lost.[130] In May 2014, Gunn added that using the songs from the 70s and 80s were 'cultural reference points', saying, 'It’s striking the balance throughout the whole movie, through something that is very unique, but also something that is easily accessible to people at the same time. The music and the Earth stuff is one of those touchstones that we have to remind us that, yeah, [Quill] is a real person from planet Earth who’s just like you and me. Except that he’s in this big outer space adventure.'[121]"
+    spyOn(paragraphFitter, 'findNextLineEnd')
+    paragraphFitter.fitToWidth(widthInInches, paragraphText)
+    expect(paragraphFitter.findNextLineEnd).toHaveBeenCalled();
   });
 });
